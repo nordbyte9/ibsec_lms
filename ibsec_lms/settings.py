5d7ea637d520876d1110f8e1f3ae49edef7bc20d
nordbyte9 <repo.mail1@tutamail.com>
2026-04-10 22:41:59 +0500

    chore: prepare settings for environment variables

--- ibsec_lms/settings.py
+++ ibsec_lms/settings.py
@@ -3,9 +3,9 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-this-in-production'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-this-in-production')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
