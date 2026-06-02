a9c26e1e38c3fcdf2b7552d9047659d87adec6c7
nordbyte9 <repo.mail1@tutamail.com>
2026-06-03 03:52:33 +0500

    feat(quizzes): track quiz attempts and results

--- quizzes/forms.py
+++ quizzes/forms.py
@@ -1,15 +1,19 @@
 from django import forms
-from .models import Quiz, Question, Option
+
+from .models import Option, Question, Quiz
+
 
 class QuizForm(forms.ModelForm):
     class Meta:
         model = Quiz
-        fields = ['course', 'title', 'description', 'pass_score', 'is_active']
+        fields = ['course', 'title', 'description', 'pass_score', 'time_limit_minutes', 'max_attempts', 'is_active']
+
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['quiz', 'text']
+
 
 class OptionForm(forms.ModelForm):
     class Meta:
