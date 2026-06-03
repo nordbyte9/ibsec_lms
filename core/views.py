0cc6cfcb0eeb45895b43fa056a693acc5696d36e
nordbyte9 <repo.mail1@tutamail.com>
2026-06-04 03:38:07 +0500

    feat(core): add system help page

--- core/views.py
+++ core/views.py
@@ -1,4 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from .navigation import breadcrumbs
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+@login_required
+def help_page(request):
+    return render(
+        request,
+        'core/help.html',
+        {
+            'breadcrumbs': breadcrumbs(('Главная', '/'), ('Справка', None)),
+        },
+    )
